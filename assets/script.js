d7ca97c520b75e9cc542a9bad81cf5c43b90de9d
Zach <zach.Brewer.1995@gmail.com>
2023-11-26 21:33:23 -0500

    Added placeholder values to cards

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,22 @@
-// Fetch request for weather forecast
-function getGeoWeather(lat, lon) {
-  fetch('api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=e869921654196b8684a99705332290bf&units=imperial')
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      console.log(data);
-    })
-};
 // Fetch request for city data
 function getCityGeoData(city) {
-  fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=e869921654196b8684a99705332290bf')
+  fetch('http://api.openweathermap.org/geo/1.0/direct?appid=e869921654196b8684a99705332290bf&limit=1&q=' + city)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+    getGeoWeather(data[0].lat, data[0].lon);
+  })
+};
+
+// Fetch request for weather forecast
+function getGeoWeather(lat, lon) {
+  fetch('api.openweathermap.org/data/2.5/forecast?appid=e869921654196b8684a99705332290bf&lat=' + lat + '&lon=' + lon + '&units=imperial')
     .then(function(response) {
       return response.json();
     })
     .then(function(data) {
       console.log(data);
-      getGeoWeather(data[0].lat, data[0].lon);
     })
 };
