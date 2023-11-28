1ffb91ba1290961e34da0aaef5ad63d7a688163e
Zach <zach.Brewer.1995@gmail.com>
2023-11-28 14:03:02 -0500

    added day 3|day 4|day 5 weather data variables

--- assets/script.js
+++ assets/script.js
@@ -16,7 +16,8 @@ function getCityGeoData(city) {
   })
   .then(function(data) {
     console.log(data);
-    getGeoWeather(data[0].lat, data[0].lon);
+    
+    
   })
 };
 
@@ -59,11 +60,10 @@ function getWeekForcast(lat, lon) {
     })
     .then(function(data) {
       console.log(data);
+
 // Variables for day 1 weather data
       var dateDay1 = data.list[11].dt_txt;
       console.log(dateDay1)
-      var weatherDay1 = data.list[11].weather;
-      console.log(weatherDay1)
       var tempDay1 = data.list[11].main.temp;
       console.log(tempDay1)
       var windDay1 = data.list[11].wind.speed;
@@ -71,7 +71,7 @@ function getWeekForcast(lat, lon) {
       var humidDay1 = data.list[11].main.humidity;
       console.log(humidDay1)
       var iconDay1 = data.list[11].weather[0].icon;
-// Applies text content for day 1 card
+// Applies text content to day 1 card
       document.getElementById('card1-date').textContent.dayjs(dateDay1).format('M/D/YYYY');
       document.getElementById('card1-icon').setAttribute('src', 'http://openweathermap.org/img/w/' + iconDay1 + '.png')
       document.getElementById('card1-temp').textContent = 'Temp: ' + tempDay1;
@@ -80,21 +80,70 @@ function getWeekForcast(lat, lon) {
       
 // Variables for day 2 weather data
       var dateDay2 = data.list[19].dt_txt;
-      console.log(dateDay1)
-      var weatherDay2 = data.list[19].weather;
-      console.log(weatherDay1)
+      console.log(dateDay2)
       var tempDay2 = data.list[19].main.temp;
-      console.log(tempDay1)
+      console.log(tempDay2)
       var windDay2 = data.list[19].wind.speed;
-      console.log(windDay1)
+      console.log(windDay2)
       var humidDay2 = data.list[19].main.humidity;
-      console.log(humidDay1)
+      console.log(humidDay2)
       var iconDay2 = data.list[19].weather[0].icon;
-// Applies text content for day 2 card
+// Applies text content to day 2 card
       document.getElementById('card1-date').textContent.dayjs(dateDay2).format('M/D/YYYY');
       document.getElementById('card1-icon').setAttribute('src', 'http://openweathermap.org/img/w/' + iconDay2 + '.png')
       document.getElementById('card1-temp').textContent = 'Temp: ' + tempDay2;
       document.getElementById('card1-wind').textContent = 'Wind: ' + windDay2;
       document.getElementById('card1-humid').textContent = 'Humidity: ' + humidDay2;
+
+// Variables for day 3 weather data
+      var dateDay3 = data.list[27].dt_txt;
+      console.log(dateDay3)
+      var tempDay3 = data.list[27].main.temp;
+      console.log(tempDay3)
+      var windDay3 = data.list[27].wind.speed;
+      console.log(windDay3)
+      var humidDay3 = data.list[27].main.humidity;
+      console.log(humidDay3)
+      var iconDay3 = data.list[27].weather[0].icon;
+// Applies text content to day 3 card
+      document.getElementById('card1-date').textContent.dayjs(dateDay3).format('M/D/YYYY');
+      document.getElementById('card1-icon').setAttribute('src', 'http://openweathermap.org/img/w/' + iconDay3 + '.png')
+      document.getElementById('card1-temp').textContent = 'Temp: ' + tempDay3;
+      document.getElementById('card1-wind').textContent = 'Wind: ' + windDay3;
+      document.getElementById('card1-humid').textContent = 'Humidity: ' + humidDay3;
+
+// Variables for day 4 weather data
+      var dateDay4 = data.list[35].dt_txt;
+      console.log(dateDay4)
+      var tempDay4 = data.list[35].main.temp;
+      console.log(tempDay4)
+      var windDay4 = data.list[35].wind.speed;
+      console.log(windDay4)
+      var humidDay4 = data.list[35].main.humidity;
+      console.log(humidDay4)
+      var iconDay4 = data.list[35].weather[0].icon;
+// Applies text content to day 4 card
+      document.getElementById('card1-date').textContent.dayjs(dateDay4).format('M/D/YYYY');
+      document.getElementById('card1-icon').setAttribute('src', 'http://openweathermap.org/img/w/' + iconDay4 + '.png')
+      document.getElementById('card1-temp').textContent = 'Temp: ' + tempDay4;
+      document.getElementById('card1-wind').textContent = 'Wind: ' + windDay4;
+      document.getElementById('card1-humid').textContent = 'Humidity: ' + humidDay4;
+
+// Variables for day 5 weather data
+      var dateDay5 = data.list[39].dt_txt;
+      console.log(dateDay5)
+      var tempDay5 = data.list[39].main.temp;
+      console.log(tempDay5)
+      var windDay5 = data.list[39].wind.speed;
+      console.log(windDay5)
+      var humidDay5 = data.list[39].main.humidity;
+      console.log(humidDay5)
+      var iconDay5 = data.list[39].weather[0].icon;
+// Applies text content to day 5 card
+      document.getElementById('card1-date').textContent.dayjs(dateDay5).format('M/D/YYYY');
+      document.getElementById('card1-icon').setAttribute('src', 'http://openweathermap.org/img/w/' + iconDay5 + '.png')
+      document.getElementById('card1-temp').textContent = 'Temp: ' + tempDay5;
+      document.getElementById('card1-wind').textContent = 'Wind: ' + windDay5;
+      document.getElementById('card1-humid').textContent = 'Humidity: ' + humidDay5;
     })
 }
