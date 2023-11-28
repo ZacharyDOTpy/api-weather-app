b715a2b7ebc98e9095d1e871927fa959de1897f1
Zach <zach.Brewer.1995@gmail.com>
2023-11-28 13:51:47 -0500

    added fetch request for week forecast and variables for day 1 & day 2 cards

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,13 @@
+var date = dayjs().format();
+
+var currentCity = document.getElementById('main-card-city');
+var currentDate = document.getElementById('main-card-date');
+var currentTemp = document.getElementById('main-card-temp');
+var currentWind = document.getElementById('main-card-wind');
+var currentIcon = document.getElementById('main-card-icon');
+var currentHumid = document.getElementById('main-card-humid');
+
+
 // Fetch request for city data
 function getCityGeoData(city) {
   fetch('http://api.openweathermap.org/geo/1.0/direct?appid=e869921654196b8684a99705332290bf&limit=1&q=' + city)
@@ -10,9 +20,9 @@ function getCityGeoData(city) {
   })
 };
 
-// Fetch request for weather forecast
+// Fetch request for current weather forecast
 function getGeoWeather(lat, lon) {
-  fetch('api.openweathermap.org/data/2.5/forecast?appid=e869921654196b8684a99705332290bf&lat=' + lat + '&lon=' + lon + '&units=imperial')
+  fetch('http://api.openweathermap.org/data/2.5/weather?appid=e869921654196b8684a99705332290bf&lat=' + lat + '&lon=' + lon + '&units=imperial')
     .then(function(response) {
       return response.json();
     })
@@ -32,13 +42,59 @@ function getGeoWeather(lat, lon) {
       console.log(humidInfo)
 
       var iconInfo = data.weather[0].icon;
-
+// Applies text content to main card
+      currentCity.textContent = data.name;
+      currentDate.textContent = dayjs().format('MMM/DD/YYYY');
+      currentTemp.textContent = 'Temp: ' + tempInfo;
+      currentWind.textContent = 'Wind: ' + windInfo;
+      currentHumid.textContent = 'Humidity: ' + humidInfo;
+      currentIcon.setAttribute('src', 'http://openweathermap.org/img/w/' + iconInfo + '.png'); 
     })
 };
-
-/* <div class="card-body" id="card1">
-  <h5 class="card-title" id="card1-date">Card title</h5>
-    <p class="card-text" id="card1-icon"></p>
-    <p class="card-text" id="card1-temp">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <p class="card-text" id="card1-wind"></p>
-    <p class="card-text" id="card1-humid"></p> */
+// Fetch request for weeks weather forecast
+function getWeekForcast(lat, lon) {
+  fetch('http://api.openweathermap.org/data/2.5/forecast?appid=e869921654196b8684a99705332290bf&lat=' + lat + '&lon=' + lon + '&units=imperial')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+// Variables for day 1 weather data
+      var dateDay1 = data.list[11].dt_txt;
+      console.log(dateDay1)
+      var weatherDay1 = data.list[11].weather;
+      console.log(weatherDay1)
+      var tempDay1 = data.list[11].main.temp;
+      console.log(tempDay1)
+      var windDay1 = data.list[11].wind.speed;
+      console.log(windDay1)
+      var humidDay1 = data.list[11].main.humidity;
+      console.log(humidDay1)
+      var iconDay1 = data.list[11].weather[0].icon;
+// Applies text content for day 1 card
+      document.getElementById('card1-date').textContent.dayjs(dateDay1).format('M/D/YYYY');
+      document.getElementById('card1-icon').setAttribute('src', 'http://openweathermap.org/img/w/' + iconDay1 + '.png')
+      document.getElementById('card1-temp').textContent = 'Temp: ' + tempDay1;
+      document.getElementById('card1-wind').textContent = 'Wind: ' + windDay1;
+      document.getElementById('card1-humid').textContent = 'Humidity: ' + humidDay1;
+      
+// Variables for day 2 weather data
+      var dateDay2 = data.list[19].dt_txt;
+      console.log(dateDay1)
+      var weatherDay2 = data.list[19].weather;
+      console.log(weatherDay1)
+      var tempDay2 = data.list[19].main.temp;
+      console.log(tempDay1)
+      var windDay2 = data.list[19].wind.speed;
+      console.log(windDay1)
+      var humidDay2 = data.list[19].main.humidity;
+      console.log(humidDay1)
+      var iconDay2 = data.list[19].weather[0].icon;
+// Applies text content for day 2 card
+      document.getElementById('card1-date').textContent.dayjs(dateDay2).format('M/D/YYYY');
+      document.getElementById('card1-icon').setAttribute('src', 'http://openweathermap.org/img/w/' + iconDay2 + '.png')
+      document.getElementById('card1-temp').textContent = 'Temp: ' + tempDay2;
+      document.getElementById('card1-wind').textContent = 'Wind: ' + windDay2;
+      document.getElementById('card1-humid').textContent = 'Humidity: ' + humidDay2;
+    })
+}
