da3621e811fd51cd08a5dabc7c35e09dcc7891c7
Zach <zach.Brewer.1995@gmail.com>
2023-11-28 13:11:34 -0500

    added variables for current day weather data

--- assets/script.js
+++ assets/script.js
@@ -18,5 +18,27 @@ function getGeoWeather(lat, lon) {
     })
     .then(function(data) {
       console.log(data);
+// Variables for current days weather data
+      var weatherInfo = data.weather;
+      console.log(weatherInfo)
+
+      var tempInfo = data.main.temp;
+      console.log(tempInfo)
+
+      var windInfo = data.wind.speed;
+      console.log(windInfo)
+
+      var humidInfo = data.main.humidity;
+      console.log(humidInfo)
+
+      var iconInfo = data.weather[0].icon;
+
     })
 };
+
+/* <div class="card-body" id="card1">
+  <h5 class="card-title" id="card1-date">Card title</h5>
+    <p class="card-text" id="card1-icon"></p>
+    <p class="card-text" id="card1-temp">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <p class="card-text" id="card1-wind"></p>
+    <p class="card-text" id="card1-humid"></p> */
